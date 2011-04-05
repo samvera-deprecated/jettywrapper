bf565cbc0b9776f01420a83f93f050abf887039c
Bess Sadler <bess@stanford.edu>
2011-04-05 15:40:46 -0700

    Adding more basic behavior

--- lib/hydra-testing.rb
+++ lib/hydra-testing.rb
@@ -4,12 +4,48 @@ module Hydra
 
       require 'singleton'
       include Singleton
-      attr_accessor :port, :jetty_home, :solr_home, :quiet
+      attr_accessor :port, :jetty_home, :solr_home, :quiet, :fedora_home
 
       # configure the singleton with some defaults
       def initialize
         @pid = nil
       end
+      
+      class << self
+        def configure(params = {})
+          hydra_server = self.instance
+          hydra_server.quiet = params[:quiet].nil? ? true : params[:quiet]
+          if defined?(Rails.root)
+            base_path = Rails.root
+          else
+            raise "You must set either RAILS_ROOT or :jetty_home so I know where jetty is" unless params[:jetty_home]
+          end
+          hydra_server.jetty_home = params[:jetty_home] || File.expand_path(File.join(base_path, 'jetty'))
+          hydra_server.solr_home = params[:solr_home]  || File.join( hydra_server.jetty_home, "solr")
+          hydra_server.fedora_home = params[:fedora_home] || File.join( hydra_server.jetty_home, "fedora","default")
+          hydra_server.port = params[:jetty_port] || 8888
+          return hydra_server
+        end
+        
+        def wrap(params = {})
+          error = false
+          hydra_server = self.configure(params)
+          begin
+            puts "starting Hydra jetty server on #{RUBY_PLATFORM}"
+            hydra_server.start
+            sleep params[:startup_wait] || 5
+            yield
+          rescue
+            error = true
+          ensure
+            puts "stopping Hydra jetty server"
+            hydra_server.stop
+          end
+
+          return error
+        end
+        
+      end #end of class << self
 
     end
   end
--- spec/lib/hydra-testing-spec.rb
+++ spec/lib/hydra-testing-spec.rb
@@ -5,6 +5,53 @@ module Hydra
   module Testing
     describe TestServer do
       
+      before(:all) do
+        RAILS_ROOT = "/path/to/rails/root"
+        @jetty_params = {
+          :quiet => false,
+          :jetty_home => "/path/to/jetty",
+          :jetty_port => 8888,
+          :solr_home => '/path/to/solr',
+          :fedora_home => '/path/to/fedora'
+        }
+      end
+      
+      it "can be instantiated" do
+        ts = Hydra::Testing::TestServer.instance
+        ts.class.should eql(Hydra::Testing::TestServer)
+      end
+      
+      it "can be configured with a params hash" do
+        ts = Hydra::Testing::TestServer.configure(@jetty_params) 
+        ts.quiet.should == false
+        ts.jetty_home.should == "/path/to/jetty"
+        ts.port.should == 8888
+        ts.solr_home.should == '/path/to/solr'
+        ts.fedora_home.should == '/path/to/fedora'
+      end
+
+      it "raises an error when called without a :jetty_home value" do
+          lambda { ts = Hydra::Testing::TestServer.configure }.should raise_exception
+          
+      end
+
+      it "should override nil params with defaults" do
+        jetty_params = {
+          :quiet => nil,
+          :jetty_home => '/path/to/jetty',
+          :jetty_port => nil,
+          :solr_home => nil,
+          :fedora_home => nil
+        }
+
+        ts = Hydra::Testing::TestServer.configure(jetty_params) 
+        ts.quiet.should == true
+        ts.jetty_home.should == "/path/to/jetty"
+        ts.port.should == 8888
+        ts.solr_home.should == File.join(ts.jetty_home, "solr")
+        ts.fedora_home.should == File.join(ts.jetty_home, "fedora","default")
+      end
+      
     end
   end
 end
