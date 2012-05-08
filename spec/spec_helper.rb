863f1caf3464da888af026473ffc3088e67b40f5
Chris Beer <chris@cbeer.info>
2012-05-08 13:52:00 -0700

    fix test jetty ports env detection under non 1.9 rubies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,8 @@ Spec::Runner.configure do |config|
   config.mock_with :mocha
 end
 
-unless ENV.select { |x| x =~ /TEST_JETTY_PORT/ }.empty?
-  TEST_JETTY_PORTS = ENV.select { |x| x =~ /TEST_JETTY_PORT/ }.sort_by { |k,v| k }.map { |k,v| v }
+unless ENV.select { |k,v| k =~ /TEST_JETTY_PORT/ }.empty?
+  TEST_JETTY_PORTS = ENV.select { |k,v| k =~ /TEST_JETTY_PORT/ }.sort_by { |k,v| k }.map { |k,v| v }
 else
   TEST_JETTY_PORTS = [8983, 8984,9999,8888]
 end
